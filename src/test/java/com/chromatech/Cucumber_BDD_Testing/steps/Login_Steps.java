4e43af86da3106f223d4d0815ce6f3bf3395f2b4
Bennett WKM <bennettwkm@gmail.com>
2024-07-16 23:12:23 -0500

    Creating Login Page

---
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/steps/Login_Steps.java
@@ -0,0 +1,40 @@
+package com.chromatech.Cucumber_BDD_Testing.steps;
+
+import com.chromatech.Cucumber_BDD_Testing.appsCommon.PageInitializer;
+import com.chromatech.utils.CommonMethods;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static com.chromatech.utils.WebDriverUtils.driver;
+
+public class Login_Steps {
+
+    PageInitializer pageInitializer = new PageInitializer();
+
+    @Given("a Chroma Tech Academy teacher or admin is on the login page {string}")
+    public void a_chroma_tech_academy_teacher_or_admin_is_on_the_login_page(String url) {
+        driver.get(url);
+    }
+
+    @When("the user enters username {string} in username text box")
+    public void the_user_enters_username_in_username_text_box(String usernameText) {
+        CommonMethods.sendKeys(pageInitializer.usernameTextBox, usernameText);
+    }
+
+    @When("enters password {string} in the password text box")
+    public void enters_password_in_the_password_text_box(String passwordText) {
+        CommonMethods.sendKeys(pageInitializer.passwordTextBox, passwordText);
+    }
+
+    @When("clicks on Sign In button")
+    public void clicks_on_sign_in_button() {
+        CommonMethods.click(pageInitializer.signInButton);
+    }
+
+    @Then("the user is successfully directed to the dashboard page {string}")
+    public void the_user_is_successfully_directed_to_the_dashboard_page(String expectedUrl) {
+        String actualUrl = driver.getCurrentUrl();
+        CommonMethods.assertEquals(actualUrl, expectedUrl);
+    }
+}
