c9b0b2394e28b7a1eb9d42839be7ac0c1c198938
IrinaEu <irina.euphoriya@gmail.com>
2024-07-18 14:36:37 -0400

    Student Admission

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/StudentAdmissionPage.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/StudentAdmissionPage.java
@@ -3,6 +3,7 @@ package com.chromatech.Cucumber_BDD_Testing.appsCommon;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
 import static com.chromatech.utils.WebDriverUtils.driver;
 
 public class StudentAdmissionPage {
