26714f9fa7d130cbde173cd1ddb1d0f4fbd98170
yu-eltech <eltech4u@gmail.com>
2024-07-20 09:45:57 -0400

    The latest saving

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/AdmittingMultipleStudentsPage.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/AdmittingMultipleStudentsPage.java
@@ -5,13 +5,9 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import static com.chromatech.utils.WebDriverUtils.driver;
 
-public class VerifyingStudentDataPage {
+public class AdmittingMultipleStudentsPage {
 
-    public VerifyingStudentDataPage() {
+    public AdmittingMultipleStudentsPage() {
         PageFactory.initElements(driver, this);
     }
-
-    /*STUDENT DETAILS SUBMODULE*/
-    @FindBy(xpath = "//li[@class='active']//a[normalize-space()='Student Details']")
-    public WebElement studentDetailsSubmodule;
 }
