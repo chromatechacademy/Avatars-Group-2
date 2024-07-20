a9dc737a0038cba32aa8653fa1b839b40e7a5c3b
OleksandraMalii <sashaproskurina09@gmail.com>
2024-07-19 19:34:44 -0500

    Code for Human Resource module(changed tag in Runner class to Regression)

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/steps/Human_Resource_module_Steps.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/steps/Human_Resource_module_Steps.java
@@ -1,6 +1,6 @@
 package com.chromatech.Cucumber_BDD_Testing.steps;
 
-import com.chromatech.Cucumber_BDD_Testing.appsCommon.HumanResourceModule;
+import com.chromatech.Cucumber_BDD_Testing.pages.HumanResourceModule;
 import com.chromatech.utils.CommonMethods;
 import com.chromatech.utils.CucumberLogUtils;
 import com.chromatech.utils.JavascriptMethods;
@@ -13,22 +13,23 @@ public class Human_Resource_module_Steps {
 
     @When("the user clicks on the Human Resource module")
     public void the_user_clicks_on_the_human_resource_module() {
-        JavascriptMethods.scrollDown(200);
+        JavascriptMethods.scrollIntoView(humanResourceModule.humanResourceButton);
         CommonMethods.waitForClickability(humanResourceModule.humanResourceButton);
         humanResourceModule.humanResourceButton.click();
         CucumberLogUtils.logScreenShot();
     }
 
     @Then("the following submodules are displayed: {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}")
-    public void the_following_submodules_are_displayed(String staffDirectorySubModule, String staffAttendanceSubModule, String payrollSubModule, String approveLeaveRequestSubModule, String applyLeaveSubModule, String leaveTypeSubModule, String teachersRatingSubModule, String departmentSubModule, String designationSubModule) {
-        CommonMethods.assertEquals(humanResourceModule.staffDirectorySubModule.getText(), "Staff Directory");
-        CommonMethods.assertEquals(humanResourceModule.staffAttendanceSubModule.getText(), "Staff Attendance");
-        CommonMethods.assertEquals(humanResourceModule.payrollSubModule.getText(), "Payroll");
-        CommonMethods.assertEquals(humanResourceModule.approveLeaveRequestSubModule.getText(), "Approve Leave Request");
-        CommonMethods.assertEquals(humanResourceModule.applyLeaveSubModule.getText(), "Apply Leave");
-        CommonMethods.assertEquals(humanResourceModule.leaveTypeSubModule.getText(), "Leave Type");
-        CommonMethods.assertEquals(humanResourceModule.teachersRatingSubModule.getText(), "Teachers Rating");
-        CommonMethods.assertEquals(humanResourceModule.departmentSubModule.getText(), "Department");
-        CommonMethods.assertEquals(humanResourceModule.designationSubModule.getText(), "Designation");
+    public void the_following_submodules_are_displayed(String expectedStaffDirectoryText, String expectedStaffAttendanceText, String expectedPayrollText, String expectedApproveLeaveRequestText, String expectedApplyLeaveText, String expectedLeaveTypeText, String expectedTeachersRatingText, String expectedDepartmentText, String expectedDesignationText) {
+        CucumberLogUtils.logScreenShot();
+        CommonMethods.assertEquals(humanResourceModule.staffDirectorySubModule.getText(), expectedStaffDirectoryText);
+        CommonMethods.assertEquals(humanResourceModule.staffAttendanceSubModule.getText(), expectedStaffAttendanceText);
+        CommonMethods.assertEquals(humanResourceModule.payrollSubModule.getText(), expectedPayrollText);
+        CommonMethods.assertEquals(humanResourceModule.approveLeaveRequestSubModule.getText(), expectedApproveLeaveRequestText);
+        CommonMethods.assertEquals(humanResourceModule.applyLeaveSubModule.getText(), expectedApplyLeaveText);
+        CommonMethods.assertEquals(humanResourceModule.leaveTypeSubModule.getText(), expectedLeaveTypeText);
+        CommonMethods.assertEquals(humanResourceModule.teachersRatingSubModule.getText(), expectedTeachersRatingText);
+        CommonMethods.assertEquals(humanResourceModule.departmentSubModule.getText(), expectedDepartmentText);
+        CommonMethods.assertEquals(humanResourceModule.designationSubModule.getText(), expectedDesignationText);
     }
 }
