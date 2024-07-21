d29170e378de82de41aff0ca3ccbc01c9a396dfe
Divyadivya11 <120313971+Divyadivya11@users.noreply.github.com>
2024-07-20 23:10:12 -0400

    AG2CP-4 - Student Information Module

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/steps/Student_Information_Module_Steps.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/steps/Student_Information_Module_Steps.java
@@ -3,21 +3,13 @@ package com.chromatech.Cucumber_BDD_Testing.steps;
 import com.chromatech.Cucumber_BDD_Testing.pages.StudentInformationModulePage;
 import com.chromatech.utils.CommonMethods;
 import com.chromatech.utils.CucumberLogUtils;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import java.util.List;
-import static com.chromatech.utils.WebDriverUtils.driver;
 
 public class Student_Information_Module_Steps {
 
     StudentInformationModulePage studentInformationModulePage = new StudentInformationModulePage();
-
-    @Given("a CTSMS user is on the dashboard page {string}")
-    public void a_CTMS_user_is_on_the_dashboard_page(String url) {
-        driver.get(url);
-        CucumberLogUtils.logScreenShot();
-    }
 
     @When("the user clicks on the Student Information module")
     public void the_user_clicks_on_the_student_information_module() {
@@ -27,12 +19,12 @@ public class Student_Information_Module_Steps {
 
     @Then("the following submodules display:")
     public void the_following_submodules_display(List<String> expectedSubmodules) {
+        CucumberLogUtils.logScreenShot();
         List<String> actualSubmodules = studentInformationModulePage.getStudentSubModulesText();
         for (int i = 0; i < expectedSubmodules.size(); i++) {
             String expectedSubModuleText = expectedSubmodules.get(i);
             String actualSubModuleText = actualSubmodules.get(i);
             CommonMethods.assertEquals(actualSubModuleText, expectedSubModuleText);
         }
-        CucumberLogUtils.logScreenShot();
     }
 }
