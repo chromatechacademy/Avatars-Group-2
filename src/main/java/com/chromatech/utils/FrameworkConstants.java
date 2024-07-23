5e612380891d3e90ecdac94fe2aad2bd4814dd6a
Olga Gonsales <olyasashams@gmail.com>
2024-07-23 18:54:58 -0400

    AG2CP-28 CTSMS ADDING EXSPENSES

--- src/main/java/com/chromatech/utils/FrameworkConstants.java
+++ src/main/java/com/chromatech/utils/FrameworkConstants.java
@@ -1,14 +1,13 @@
 package com.chromatech.utils;
 
 public class FrameworkConstants {
-
     public static final String LOCAL_TEST_SETTINGS_FILEPATH = System.getProperty("user.dir") + "/src/main/resources/config/localTestSettings.properties";
-    public static final String SCREENSHOT = System.getProperty("user.dir") + "/src/main/resources/files/Photo.png";
-    public static final String STUDENT_PHOTO_FILEPATH = System.getProperty("user.dir") + "/src/main/resources/file/Emma.png";
-    public static final String FATHER_PHOTO_FILEPATH = System.getProperty("user.dir") + "/src/main/resources/file/Carl.png";
-    public static final String MOTHER_PHOTO_FILEPATH = System.getProperty("user.dir") + "/src/main/resources/file/Helen.png";
-    public static final String DOCUMENT_ONE_FILEPATH = System.getProperty("user.dir") + "/src/main/resources/file/Doc One.docx";
-    public static final String DOCUMENT_TWO_FILEPATH = System.getProperty("user.dir") + "/src/main/resources/file/Doc Two.docx";
-    public static final String DOCUMENT_THREE_FILEPATH = System.getProperty("user.dir") + "/src/main/resources/file/Doc Three.docx";
-    public static final String DOCUMENT_FOUR_FILEPATH = System.getProperty("user.dir") + "/src/main/resources/file/Doc Four.docx";
+    public static final String SCREENSHOT = System.getProperty("user.dir") + "/src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/file/Photo.png";
+    public static final String STUDENT_PHOTO_FILEPATH = System.getProperty("user.dir") + "/src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/file/Emma.png";
+    public static final String FATHER_PHOTO_FILEPATH = System.getProperty("user.dir") + "/src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/file/Carl.png";
+    public static final String MOTHER_PHOTO_FILEPATH = System.getProperty("user.dir") + "/src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/file/Helen.png";
+    public static final String DOCUMENT_ONE_FILEPATH = System.getProperty("user.dir") + "/src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/file/Doc One.docx";
+    public static final String DOCUMENT_TWO_FILEPATH = System.getProperty("user.dir") + "/src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/file/Doc Two.docx";
+    public static final String DOCUMENT_THREE_FILEPATH = System.getProperty("user.dir") + "/src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/file/Doc Three.docx";
+    public static final String DOCUMENT_FOUR_FILEPATH = System.getProperty("user.dir") + "/src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/file/Doc Four.docx";
 }
