a092dc5292a4207dfc6bbe3406be63b9cbb98e99
Olga Gonsales <olyasashams@gmail.com>
2024-07-26 17:42:20 -0400

    Saving changes

---
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/steps/Income_Module_Steps.java
@@ -0,0 +1,26 @@
+package com.chromatech.Cucumber_BDD_Testing.steps;
+
+import com.chromatech.Cucumber_BDD_Testing.pages.IncomeModulePage;
+import com.chromatech.utils.CommonMethods;
+import com.chromatech.utils.CucumberLogUtils;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Income_Module_Steps {
+
+    IncomeModulePage incomeModulePage = new IncomeModulePage();
+
+    @When("user clicks on Income module")
+    public void user_clicks_on_income_module() {
+        CommonMethods.click(incomeModulePage.incomeModule);
+        CucumberLogUtils.logScreenShot();
+    }
+
+    @Then("the following submodules are displayed:{string}, {string}, {string}")
+    public void the_following_submodules_are_displayed(String expectedAddIncomeText, String expectedSearchIncomeText, String expectedIncomeHeadText) {
+        CucumberLogUtils.logScreenShot();
+        CommonMethods.assertEquals(incomeModulePage.addIncomeSubmodule.getText(), expectedAddIncomeText);
+        CommonMethods.assertEquals(incomeModulePage.searchIncomeSubmodule.getText(), expectedSearchIncomeText);
+        CommonMethods.assertEquals(incomeModulePage.incomeHeadSubmodule.getText(), expectedIncomeHeadText);
+    }
+}
