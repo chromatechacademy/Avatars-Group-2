8ceb41844913d2160e1cbcf49e9c7bdcaf10e79f
Divyadivya11 <120313971+Divyadivya11@users.noreply.github.com>
2024-07-28 15:19:08 -0400

    RE-RUN TESTS

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/runners/Re_Run_Failed_Test_Cases.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/runners/Re_Run_Failed_Test_Cases.java
@@ -1,9 +1,7 @@
 package com.chromatech.Cucumber_BDD_Testing.runners;
 
-import com.chromatech.Cucumber_BDD_Testing.appsCommon.RetryListener;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.Listeners;
 
 @CucumberOptions(plugin = {"html:target/cucumber-reports/cucumber-html-report.html", "json:target/cucumber.json",
         "rerun:target/failed.txt", "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
@@ -13,6 +11,5 @@ import org.testng.annotations.Listeners;
         dryRun = false
 )
 
-@Listeners(RetryListener.class)
 public class Re_Run_Failed_Test_Cases extends AbstractTestNGCucumberTests {
 }
