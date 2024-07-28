3f3867c3915422974b3412dbaba0d6cd6ea7a1a0
IrinaEu <irina.euphoriya@gmail.com>
2024-07-28 01:35:29 -0400

    AG2CP-17 CTSMS - Ability to add and delete sections

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/pages/HomeworkModulePage.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/pages/HomeworkModulePage.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.support.PageFactory;
 public class HomeworkModulePage {
 
     public HomeworkModulePage() {
-
         PageFactory.initElements(WebDriverUtils.driver, this);
     }
 
--- src/test/java/com/chromatech/Cucumber_BDD_Testing/runners/Run_CTSMS_Progression_Test.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/runners/Run_CTSMS_Progression_Test.java
@@ -3,7 +3,6 @@ package com.chromatech.Cucumber_BDD_Testing.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-
 @CucumberOptions(plugin = { "html:target/cucumber-reports/cucumber-html-report.html","json:target/cucumber.json",
         "rerun:target/failed.txt",
         "pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
